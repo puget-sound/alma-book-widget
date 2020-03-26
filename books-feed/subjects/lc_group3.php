a9af9f33235eb7fc69af301a57435a4a23614072
Hilary Robbeloth <hrobbeloth@pugetsound.edu>
2020-03-26 14:45:33 -0700

    Update lc_group3.php
    mapping to UPS disciplines

--- books-feed/subjects/lc_group3.php
+++ books-feed/subjects/lc_group3.php
@@ -7,16 +7,16 @@ foreach($group3 as $term) {
 
 	switch ($term) {
     		case "Accounting. Bookkeeping":
-        		array_push($subjects, "Accountancy");
+        		array_push($subjects, "Business & Leadership");
         		break;
 		case "Reproduction of library materials. Storage media of library materials":
-			array_push($subjects, "Archival Studies");
+			array_push($subjects, "Classics & Ancient Mediterranean Studies");
 			break;
 		case "Diseases of the respiratory system":
-                        array_push($subjects, "Cardiopulmonary Science");
+                        array_push($subjects, "Occupational Therapy");
                         break;
                 case "Career education":
-                        array_push($subjects, "Career Resources");
+                        array_push($subjects, "Education Studies");
                         break;
                 case "Vocational guidance. Career devlopment":
                         array_push($subjects, "Career Resources");
@@ -40,13 +40,13 @@ foreach($group3 as $term) {
                         array_push($subjects, "Communication Studies");
                         break;
                 case "Information technology":
-                        array_push($subjects, "Computing");
+                        array_push($subjects, "Computer Science");
                         break;
                 case "Computer engineering. Computer hardware":
-                        array_push($subjects, "Computing");
+                        array_push($subjects, "Computer Science");
                         break;
                 case "Calculating machines":
-                        array_push($subjects, "Computing");
+                        array_push($subjects, "Computer Science");
                         break;
                 case "Style. Composition. Rhetoric":
                         array_push($subjects, "English");
@@ -55,73 +55,73 @@ foreach($group3 as $term) {
                         array_push($subjects, "English");
                         break;
                 case "Women. Girls; Gays. Lesbians. Bisexuals":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "Gender & Queer Studies");
                         break;	
                 case "Transexualism":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "Gender & Queer Studies");
                         break;			
                 case "Race (General)":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "African American Studies");
                         break;	
                 case "Special classes. By race or ethnic group":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "Gender & Queer Studies");
                         break;	
                 case "Multicultural education (General)":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "Education Studies");
                         break;							
                 case "Social perception. Social cognition":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "Sociology & Anthropology");
                         break;	
                 case "Personnel management. Employment":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;							
                 case "Employee participation in management.":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;
                 case "Business records management":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;						
                 case "Supervision and administration. Business management":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;						
                 case "Organizational behavior, change and":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;						
                 case "Organizational sociology. Organization theory":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;		
                 case "Industrial productivity":
-                        array_push($subjects, "Management and Leadership");
+                        array_push($subjects, "Business & Leadership");
                         break;
 		case "Marketing. Distribution of products":
-                        array_push($subjects, "Marketing");
+                        array_push($subjects, "Business & Leadership");
                         break;
                 case "Personal health and hygiene Including clothing, bathing, exercise, travel, nutrition, sleep, sex hygiene":
-                        array_push($subjects, "Nutrition and Dietetics");
+                        array_push($subjects, "Occupational Therapy");
                         break;
                 case "Debating":
-                        array_push($subjects, "Debating");
+                        array_push($subjects, "Politics & Government");
                         break;
                 case "Government. Public Administration":
-                        array_push($subjects, "Public Administration");
+                        array_push($subjects, "Politics & Government");
                         break;
                 case "Community":
-                        array_push($subjects, "Regional and Community Studies");
+                        array_push($subjects, "Sociology & Anthropology");
                         break;
                 case "Community and the school":
-                        array_push($subjects, "Regional and Community Studies");
+                        array_push($subjects, "Education Studies");
                         break;
                 case "Neurology. Diseases of the nervous system Including speech disorders":
-                        array_push($subjects, "Speech-Language Pathology");
+                        array_push($subjects, "Neuroscience");
                         break;	
                 case "Sports medicine":
-                        array_push($subjects, "Sport Physiology and Performance");
+                        array_push($subjects, "Exercise Science");
                         break;	
                 case "Dramatic representation. The theater":
-                        array_push($subjects, "Theater");
+                        array_push($subjects, "Theatre Arts");
                         break;
                 case "Biography of women (Collective)":
-                        array_push($subjects, "Womens Studies");
+                        array_push($subjects, "Gender & Queer Studies");
                         break;		
 	}//end Switch
 }//End foreach
