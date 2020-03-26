7a4b68ee82617de4fd2c19f061c5d485754fc256
Hilary Robbeloth <hrobbeloth@pugetsound.edu>
2020-03-26 15:23:14 -0700

    Update lc_group1.php
    mapping to UPS disciplines

--- books-feed/subjects/lc_group1.php
+++ books-feed/subjects/lc_group1.php
@@ -47,7 +47,7 @@ foreach($group1 as $term) {
                         array_push($subjects, "Physics");
                         break;
 		case "Natural history (General)":
-                        array_push($subjects, "Biology");
+                        array_push($subjects, "Natural Science");
                         break;
 		case "Biology (General)":
                         array_push($subjects, "Biology");
@@ -65,7 +65,7 @@ foreach($group1 as $term) {
                         array_push($subjects, "Business & Leadership");
                         break;
                 case "Commerce":
-                        array_push($subjects, "Business & Leadership");
+                        array_push($subjects, "Economics");
                         break;
                 case "Dentistry":
                         array_push($subjects, "Occupational Therapy");
@@ -74,25 +74,25 @@ foreach($group1 as $term) {
                         array_push($subjects, "Business & Leadership");
                         break;
                 case "Economic history and conditions":
-                        array_push($subjects, "Business & Leadership");
+                        array_push($subjects, "Economics");
                         break;
                 case "Economic theory. Demography":
-                        array_push($subjects, "Business & Leadership");
+                        array_push($subjects, "Economics");
                         break;
                 case "Public finance":
-                        array_push($subjects, "Business & Leadership");
+                        array_push($subjects, "Economics");
                         break;
                 case "History of education":
-                        array_push($subjects, "Education");
+                        array_push($subjects, "Education Studies");
                         break;
                 case "Special aspects of education":
-                        array_push($subjects, "Education");
+                        array_push($subjects, "Education Studies");
                         break;
                 case "Theory and practice of education":
-                        array_push($subjects, "Education");
+                        array_push($subjects, "Education Studies");
                         break;
                 case "Electrical engineering. Electronics. Nuclear engineering":
-			array_push($subjects, "Science,  Technology & Society");
+			array_push($subjects, "Engineering");
 			break;
 		case "Environmental technology. Sanitary engineering":
                         array_push($subjects, "Environmental Policy & Decision Making");
@@ -101,22 +101,22 @@ foreach($group1 as $term) {
                         array_push($subjects, "Environmental Policy & Decision Making");
                         break;
                 case "Greek philology and language":
-                        array_push($subjects, "Classics");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Hyperborean, Indian, and artificial languages":
-                        array_push($subjects, "Asian Studies");
+                        array_push($subjects, "Asian Languages & Cultures");
                         break;
 		case "Languages of Eastern Asia, Africa, Oceania":
-                        array_push($subjects, "Asian Studies");
+                        array_push($subjects, "Asian Languages & Cultures");
                         break;
                 case "Latin philology and language":
-                        array_push($subjects, "Classics");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Medieval and modern Greek language":
-                        array_push($subjects, "Classics");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;						
                 case "Modern languages. Celtic languages":
-                        array_push($subjects, "Classics");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;						
                 case "Afrikaans literature":
                         array_push($subjects, "African American Studies");
@@ -125,88 +125,88 @@ foreach($group1 as $term) {
                         array_push($subjects, "English");
                         break;
                 case "Byzantine and modern Greek literature":
-                        array_push($subjects, "Classics");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Canadian literature":
                         array_push($subjects, "English");
                         break;
                 case "Classical literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Danish literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;						
                 case "Dutch literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;	
                 case "English literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "English");
                         break;	
                 case "Faroese literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;	
                 case "Flemish literature since 1830":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;	
                 case "French literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "French Studies");
                         break;	
                 case "German literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "German Studies");
                         break;							
                 case "Greek literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;							
                 case "Indo-Iranian philology and literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;		
                 case "Italian literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Literature (General)":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "English");
                         break;
                 case "Literature on music":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Music");
                         break;
                 case "Medieval and modern Latin literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Modern Icelandic literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;
                 case "Norwegian literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;
                 case "Old Norse literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;			
                 case "Old Icelandic and Old Norwegian":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;		
                 case "Oriental philology and literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Asian Languages & Cultures");
                         break;
                 case "Portuguese literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Latina/o Studies");
                         break;
                 case "Roman literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Classics & Ancient Mediterranean Studies");
                         break;
                 case "Scandinavian literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;
                 case "Spanish literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "Latina/o Studies");
                         break;				
                 case "Swedish literature":
-                        array_push($subjects, "Foreign Language and Literature");
+                        array_push($subjects, "History");
                         break;	
                 case "West Germanic":
-			array_push($subjects, "Foreign Language and Literature");
+			array_push($subjects, "German Studies");
 			break;
 		case "Geology":
-                        array_push($subjects, "Geosciences");
+                        array_push($subjects, "Geology");
                         break;
                 case "History (General)":
                         array_push($subjects, "History");
@@ -216,7 +216,10 @@ foreach($group1 as $term) {
                         break;							
                 case "History of Asia":
                         array_push($subjects, "History");
-			break;							
+			break;			
+                case "History of Asia":
+                        array_push($subjects, "Asian Languages & Cultures");
+			break;
                 case "History of Austria. Liechtenstein. Hungary. Czechoslovakia":
                         array_push($subjects, "History");
                         break;							
@@ -225,20 +228,28 @@ foreach($group1 as $term) {
                         break;							
                 case "History of Central Europe":
                         array_push($subjects, "History");
-                        break;							
+                        break;	
+		case "History of Central Europe":
+                        array_push($subjects, "German Studies");
+                        break;	
                 case "History of Civilization":
                         array_push($subjects, "History");
-                        array_push($subjects, "International Studies");
 			break;							
                 case "History of Eastern Europe (General)":
                         array_push($subjects, "History");
                         break;							
                 case "History of France":
                         array_push($subjects, "History");
-                        break;							
+                        break;		
+	        case "History of France":
+                        array_push($subjects, "French Studies");
+                        break;	
                 case "History of Germany":
                         array_push($subjects, "History");
-                        break;							
+                        break;	
+		case "History of Germany":
+                        array_push($subjects, "German Studies");
+                        break;
                 case "History of Great Britain":
                         array_push($subjects, "History");
                         break;							
@@ -288,49 +299,49 @@ foreach($group1 as $term) {
                         array_push($subjects, "History");
                         break;		
                 case "International law, see JZ and KZ":
-                        array_push($subjects, "International Studies");
+                        array_push($subjects, "International Political Economy");
                         break;
                 case "International relations":
-                        array_push($subjects, "International Studies");
+                        array_push($subjects, "International Political Economy");
                         break;	
                 case "Law in general. Comparative and uniform law. Jurisprudence":
-                        array_push($subjects, "Legal Resources");
+                        array_push($subjects, "Politics & Government");
                         break;	
                 case "Law of the United States":
-                        array_push($subjects, "Legal Resources");
+                        array_push($subjects, "Politics & Government");
                         break;	
                 case "Medicine (General)":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;	
                 case "Pathology":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;							
                 case "Internal medicine":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;							
                 case "Surgery":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;							
                 case "Ophthalmology":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;							
                 case "Otorhinolaryngology":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;							
                 case "Gynecology and obstetrics":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;							
                 case "Pediatrics":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;			
                 case "Dermatology":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Occupational Therapy");
                         break;		
                 case "Botanic, Thomsonian, and eclectic medicine":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Science, Technology & Society");
                         break;		
                 case "Homeopathy":
-                        array_push($subjects, "Medicine");
+                        array_push($subjects, "Science, Technology & Society");
                         break;	
                 case "Music":
                         array_push($subjects, "Music");
@@ -342,13 +353,13 @@ foreach($group1 as $term) {
                         array_push($subjects, "Music");
                         break;
                 case "Nursing":
-                        array_push($subjects, "Nursing");
+                        array_push($subjects, "Occupational Therapy");
                         break;
                 case "Pharmacy and materia medica":
-                        array_push($subjects, "Pharmacy");
+                        array_push($subjects, "Occupational Therapy");
                         break;		
                 case "Therapeutics. Pharmacology":
-                        array_push($subjects, "Pharmacy");
+                        array_push($subjects, "Occupational Therapy");
                         break;					
                 case "Philosophy (General)":
                         array_push($subjects, "Philosophy");
@@ -357,28 +368,28 @@ foreach($group1 as $term) {
                         array_push($subjects, "Physics");
                         break;
                 case "Political institutions and public administration":
-                        array_push($subjects, "Political Science");
+                        array_push($subjects, "Politics & Government");
                         break;			
                 case "Political theory. The state. Theories of the state":
-                        array_push($subjects, "Political Science");
+                        array_push($subjects, "Politics & Government");
                         break;								
                 case "Political institutions and public administration (Asia,":
-                        array_push($subjects, "Political Science");
+                        array_push($subjects, "Politics & Government");
                         break;							
                 case "Political institutions and public administration (Europe)":
-                        array_push($subjects, "Political Science");
+                        array_push($subjects, "Politics & Government");
                         break;	
                 case "Local government. Municipal government":
-                        array_push($subjects, "Political Science");
+                        array_push($subjects, "Politics & Government");
                         break;
                 case "Psychology":
                         array_push($subjects, "Psychology");
                         break;	
                 case "Communities. Classes. Races":
-			array_push($subjects, "Public Administration");
+			array_push($subjects, "African American Studies");
 			break;
 		case "Sociology":
-                        array_push($subjects, "Sociology");
+                        array_push($subjects, "Sociology & Anthropology");
                         break;			
 	}//End Switch
 }//End foreach
