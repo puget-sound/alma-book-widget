826f8de5cee544151b48484e1f689977be1f32cc
Hilary Robbeloth <hrobbeloth@pugetsound.edu>
2020-03-26 13:18:39 -0700

    Update lc_group4.php
    mapping to UPS disciplines.

--- books-feed/subjects/lc_group4.php
+++ books-feed/subjects/lc_group4.php
@@ -7,22 +7,22 @@ foreach($group4 as $term) {
 
 	switch ($term) {
     		case "Computer games. Video games. Fantasy games":
-        		array_push($subjects, "Computing");
+        		array_push($subjects, "Computer Science");
         		break;
                 case "Computer software":
-                        array_push($subjects, "Computing");
+                        array_push($subjects, "Computer Science");
                         break;
                 case "Computer-assisted education":
-                        array_push($subjects, "Computing");
+                        array_push($subjects, "Education Studies");
                         break;
                 case "Electronic computers. Computer science":
-                        array_push($subjects, "Computing");
+                        array_push($subjects, "Computer Science");
                         break;
                 case "Computer games. Video games. Fantasy games":
-                        array_push($subjects, "Digital Media");
+                        array_push($subjects, "Computer Science");
                         break;
                case "Women":
-                        array_push($subjects, "Gender and Diversity");
+                        array_push($subjects, "Gender & Queer Studies");
                         break;
     		default:
         	//	array_push($subjects, "default test");
