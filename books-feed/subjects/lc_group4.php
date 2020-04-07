cd738d58523273885bfffbadf7e4518ca49d0355
Hilary Robbeloth <hrobbeloth@pugetsound.edu>
2020-04-07 08:23:08 -0700

    Update lc_group4.php
    added more group 4 terms from Analytics

--- books-feed/subjects/lc_group4.php
+++ books-feed/subjects/lc_group4.php
@@ -6,24 +6,55 @@
 foreach($group4 as $term) {
 
 	switch ($term) {
-    		case "Computer games. Video games. Fantasy games":
-        		array_push($subjects, "Computer Science");
-        		break;
-                case "Computer software":
-                        array_push($subjects, "Computer Science");
-                        break;
-                case "Computer-assisted education":
-                        array_push($subjects, "Education Studies");
-                        break;
-                case "Electronic computers. Computer science":
-                        array_push($subjects, "Computer Science");
-                        break;
-                case "Computer games. Video games. Fantasy games":
-                        array_push($subjects, "Computer Science");
-                        break;
-               case "Women":
-                        array_push($subjects, "Gender & Queer Studies");
-                        break;
+		case "Status and development since emancipation":       array_push($subjects, "African American Studies");       break;
+		case "Biography. Genealogy":       array_push($subjects, "History");       break;
+		case "History":       array_push($subjects, "History");       break;
+		case "Hong Kong":       array_push($subjects, "Asian Studies");       break;
+		case "Legislation. Legislative process. Law-making":       array_push($subjects, "Politics & Government");       break;
+		case "The stage and accessories":       array_push($subjects, "Theatre Arts");       break;
+		case "Therapeutics. Psychotherapy":       array_push($subjects, "Psychology");       break;
+		case "19th century":       array_push($subjects, "History");       break;
+		case "Biography (General)":       array_push($subjects, "History");       break;
+		case "Electoral system":       array_push($subjects, "Politics & Government");       break;
+		case "Individual and state":       array_push($subjects, "Politics & Government");       break;
+		case "Islamic religious life":       array_push($subjects, "Religious Studies");       break;
+		case "Muhammad, Prophet, d. 632":       array_push($subjects, "Religious Studies");       break;
+		case "Musical sources":       array_push($subjects, "Music");       break;
+		case "Psychopathology":       array_push($subjects, "Psychology");       break;
+		case "Children":       array_push($subjects, "Education Studies");       break;
+		case "Ethnography":       array_push($subjects, "Sociology & Anthropology");       break;
+		case "Groundwater. Hydrogeology":       array_push($subjects, "Geology");       break;
+		case "Hadith literature. Traditions. Sunna":       array_push($subjects, "Religious Studies");       break;
+		case "Modern, 1515-":       array_push($subjects, "History");       break;
+		case "20th century":       array_push($subjects, "History");       break;
+		case "Academic freedom":       array_push($subjects, "Education Studies");       break;
+		case "Antiquities. Social life and customs. Ethnography":       array_push($subjects, "Classics & Ancient Mediterranean Studies");       break;
+		case "Arctic regions":       array_push($subjects, "Sociology & Anthropology");       break;
+		case "Babylonian Talmud":       array_push($subjects, "Religious Studies");       break;
+		case "Biography":       array_push($subjects, "History");       break;
+		case "Biography (Late eighteenth century)":       array_push($subjects, "History");       break;
+		case "Black Muslims":       array_push($subjects, "Religious Studies");       break;
+		case "Black Muslims":       array_push($subjects, "African American Studies");       break;
+		case "Business associations":       array_push($subjects, "Business & Leadership");       break;
+		case "Children and youth with disabilities. Learning disabled children and youth":       array_push($subjects, "Education Studies");       break;
+		case "Children and youth with disabilities. Learning disabled children and youth":       array_push($subjects, "Psychology");       break;
+		case "China (Peoples Republic, 1949- )":       array_push($subjects, "Asian Studies");       break;
+		case "Congregationalism":       array_push($subjects, "Religious Studies");       break;
+		case "Criminal law":       array_push($subjects, "Politics & Government");       break;
+		case "Discrimination in education":       array_push($subjects, "Education Studies");       break;
+		case "Discrimination in education":       array_push($subjects, "Sociology & Anthropology");       break;
+		case "Early and medieval":       array_push($subjects, "History");       break;
+		case "Early nineteenth century":       array_push($subjects, "History");       break;
+		case "Individual Old Testament characters":       array_push($subjects, "Religious Studies");       break;
+		case "Islamic education":       array_push($subjects, "Religious Studies");       break;
+		case "One solo voice":       array_push($subjects, "Music");       break;
+		case "Psychoanalysis":       array_push($subjects, "Psychology");       break;
+		case "Puritanism":       array_push($subjects, "Religious Studies");       break;
+		case "Religious orders of men":       array_push($subjects, "Religious Studies");       break;
+		case "Shiites":       array_push($subjects, "Religious Studies");       break;
+		case "Suffrage":       array_push($subjects, "Gender & Queer Studies");       break;
+		case "Television broadcasts":       array_push($subjects, "Communication Studies");       break;
+		case "Zen Buddhism":       array_push($subjects, "Religious Studies");       break;
     		default:
         	//	array_push($subjects, "default test");
 	}//End switch
